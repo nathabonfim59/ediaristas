74e17ad6072841939322e3ed8a2c4febeda6acb0
nathabonfim59 <nathabonfim59@gmail.com>
2021-10-08 02:16:07 -0300

    add(componente): UserInformation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,12 @@ const Home: NextPage = () => {
       <PageTitle
         title={'Conheça os profissionais'}
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+      />
+      <UserInformation
+        name={'Nathanael'}
+        picture={'https://github.com/nathabonfim59.png'}
+        rating={5}
+        description={'Um desenvolvedor apaixonado.'}
       />
     </div>
   )
